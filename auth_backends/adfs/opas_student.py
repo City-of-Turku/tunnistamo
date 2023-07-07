319855fae3a3b64be2a4450d7428098fa3cbb7f2
Timo Hanski <timo.hanski@haltu.fi>
2023-07-07 12:34:05 +0300

    Prevent keyError by adding fallback None when getting school_role

--- auth_backends/adfs/opas_student.py
+++ auth_backends/adfs/opas_student.py
@@ -4,7 +4,7 @@ class OpasStudentADFS(OpasADFS):
     name = 'opas_student_adfs'
 
     def auth_allowed(self, response, details):
-        role = details['school_role']
+        role = details.get('school_role', None)
         if role == 'student':
             super().auth_allowed(response, details)
         else:
